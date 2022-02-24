5d6d72187a2df322dcdf54bb56052fb762fdeddb
GrafGrihula <grafgrihula@yandex.ru>
2022-02-24 20:55:27 +0300

    ДЗ по уроку № 3

---
+++ LessonCSharp_03_WebApp/Timesheets/Controllers/ContractsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Timesheets.Controllers
+{
+    [Route("api/contracts")]
+    [ApiController]
+    public class ContractsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ContractsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
---
+++ LessonCSharp_03_WebApp/Timesheets/Controllers/Models/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace Timesheets.Controllers.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+    }
+}
