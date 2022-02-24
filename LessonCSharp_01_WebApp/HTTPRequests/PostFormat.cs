684f9d1a81b48a3c22bcdf30fe24b276de35b9b9
GrafGrihula <grafgrihula@yandex.ru>
2022-02-24 20:51:58 +0300

    ДЗ по уроку № 1

---
+++ LessonCSharp_01_WebApp/HTTPRequests/PostFormat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPRequests
+{
+    public class PostFormat
+    {
+        public uint userId { get; set; }
+        public uint id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public override string ToString() => $"{userId}\n{id}\n{title}\n{body}\n";
+    }
+}
